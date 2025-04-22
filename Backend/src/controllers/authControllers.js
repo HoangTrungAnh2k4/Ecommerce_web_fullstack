2c8d9c5c2898a564b13af53d7aab10560a1689d6
HoangTrungAnh <anh.hoang04k22@hcmut.edu.vn>
2025-04-22 20:48:40 +0700

    update

--- Backend/src/controllers/authControllers.js
+++ Backend/src/controllers/authControllers.js
@@ -19,6 +19,8 @@ const authControllers = {
     },
 
     login: async (req, res) => {
+        res.status(200).json({ message: 'Login successful' });
+        return;
         const { phoneNumber, password } = req.body;
 
         if (!phoneNumber || !password) {
